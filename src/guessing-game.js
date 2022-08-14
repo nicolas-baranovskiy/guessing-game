40136cb9eda020c72b14dc52dc180c40e633842f
nicolas-baranovskiy <nicolas.baranovskiy@gmail.com>
2022-08-14 20:36:53 +0300

    feat: update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,25 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        if ((this.max + this.min) % 2 === 0) {
+            this.mid = (this.max + this.min) / 2;
+        } else {
+            this.mid = Math.ceil((this.max + this.min) / 2)
+        }
+      return this.mid
     }
 
     lower() {
-
+this.max = this.mid
     }
 
     greater() {
-
+this.min = this.mid
     }
 }
 
